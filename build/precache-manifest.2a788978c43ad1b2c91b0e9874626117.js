fa87be7e0c8f61dfe595d0e8328f22cb658a7c54
soundbroo <soundbroo@gmail.com>
2019-04-20 20:56:58 +0300

    v1
    Junior Front-end test app

---
+++ build/precache-manifest.2a788978c43ad1b2c91b0e9874626117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1a075c2a9d30d2c4924",
+    "url": "/static/js/main.4f3f1f01.chunk.js"
+  },
+  {
+    "revision": "81089b292d01a835ed4b",
+    "url": "/static/js/2.fb6478f2.chunk.js"
+  },
+  {
+    "revision": "d1a075c2a9d30d2c4924",
+    "url": "/static/css/main.229e1c21.chunk.css"
+  },
+  {
+    "revision": "441365159788729529c87b64b95b0730",
+    "url": "/index.html"
+  }
+];
